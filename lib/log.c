09e7545426f75014898a89fbd1c1494427a878db
Everton Marques <everton.marques@gmail.com>
2010-04-15 15:58:30 -0300

    [pim] PIM route type.

--- lib/log.c
+++ lib/log.c
@@ -837,6 +837,7 @@ static const struct zebra_desc_table route_types[] = {
   DESC_ENTRY	(ZEBRA_ROUTE_ISIS,	"isis",		'I' ),
   DESC_ENTRY	(ZEBRA_ROUTE_BGP,	"bgp",		'B' ),
   DESC_ENTRY	(ZEBRA_ROUTE_HSLS,	"hsls",		'H' ),
+  DESC_ENTRY	(ZEBRA_ROUTE_PIM,	"pim",		'P' ),
 };
 #undef DESC_ENTRY
 
