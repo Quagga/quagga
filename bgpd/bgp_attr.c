Quagga/quagga

commit 1a211cb369dc865a4e7e9f58a100c041af457262

bgpd: one more fix for tightening of check for missing well-known attributes
* bgp_attr.c: (bgp_attr_check) The check for missing NEXT_HOP has the right
  spirit, but wrong where it counts, on the logic.  It wouldn't catch a
  missing NEXT_HOP on a v4-only UPDATE.  It would though have incorrectly
  flagged next-hop as missing on multi-protocol-only UPDATEs.

  Caught by Martin Winter with a test-suite.

bgpd/bgp_attr.c
@@ -1761,8 +1761,8 @@ bgp_attr_check (struct peer *peer, struct attr *attr)
   /* RFC 2858 makes Next-Hop optional/ignored, if MP_REACH_NLRI is present and
    * NLRI is empty. We can't easily check NLRI empty here though.
    */
-  if (CHECK_FLAG (attr->flag, ATTR_FLAG_BIT (BGP_ATTR_MP_REACH_NLRI))
-      && !CHECK_FLAG (attr->flag, ATTR_FLAG_BIT (BGP_ATTR_NEXT_HOP)))
+  if (!CHECK_FLAG (attr->flag, ATTR_FLAG_BIT (BGP_ATTR_NEXT_HOP))
+      && !CHECK_FLAG (attr->flag, ATTR_FLAG_BIT (BGP_ATTR_MP_REACH_NLRI)))
     type = BGP_ATTR_NEXT_HOP;
   
   if (peer->sort == BGP_PEER_IBGP

commit daefeb8755e194dd19a5f1910bc78d13c8147efb

bgpd: set BGP_ATTR_MP_[UN]REACH_NLRI (fixes 1a211cb)
Unfortunately, the attribute present bits for MP_REACH and MP_UNREACH
which 1a211cb ("bgpd: one more fix"...) tests for are never set in their
corresponding attribute parsing functions.

Reported-by: Martin Winter <mwinter@netdef.org>
Fixes: 1a211cb "bgpd: one more fix for tightening of check for missing well-known attributes"
Cc: Paul Jakma <paul@opensourcerouting.org>
Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

bgpd/bgp_attr.c
@@ -1591,6 +1591,8 @@ bgp_mp_reach_parse (struct bgp_attr_parser_args *args,
 
   stream_forward_getp (s, nlri_len);
 
+  attr->flag |= ATTR_FLAG_BIT (BGP_ATTR_MP_REACH_NLRI);
+
   return BGP_ATTR_PARSE_PROCEED;
 #undef LEN_LEFT
 }
@@ -1606,6 +1608,7 @@ bgp_mp_unreach_parse (struct bgp_attr_parser_args *args,
   u_int16_t withdraw_len;
   int ret;
   struct peer *const peer = args->peer;  
+  struct attr *const attr = args->attr;
   const bgp_size_t length = args->length;
 
   s = peer->ibuf;
@@ -1632,6 +1635,8 @@ bgp_mp_unreach_parse (struct bgp_attr_parser_args *args,
   mp_withdraw->length = withdraw_len;
 
   stream_forward_getp (s, withdraw_len);
+
+  attr->flag |= ATTR_FLAG_BIT (BGP_ATTR_MP_UNREACH_NLRI);
 
   return BGP_ATTR_PARSE_PROCEED;
 }