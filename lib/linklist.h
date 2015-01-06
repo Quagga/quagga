Quagga/quagga

commit 6d831139569dbee69acc631361db917e2c47feeb

ospfd: Tweak previous iface RR write patch to avoid free/malloc & redundant log
* linklist.{c,h}: (listnode_move_to_tail) new unction to move a
  listnode to tail of list.
* ospf_packet.c: (ospf_write) remove debug that seemed to be mostly covered
  by existing debug.
  Use listnode_move_to_tail to just move the list node to the end of the
  tail, rather than freeing the one to hand and allocing a new one.

lib/linklist.h
@@ -68,6 +68,7 @@ extern void list_free (struct list *);
 extern void listnode_add (struct list *, void *);
 extern void listnode_add_sort (struct list *, void *);
 extern void listnode_add_after (struct list *, struct listnode *, void *);
+extern void listnode_move_to_tail (struct list *, struct listnode *);
 extern void listnode_delete (struct list *, void *);
 extern struct listnode *listnode_lookup (struct list *, void *);
 extern void *listnode_head (struct list *);

lib/linklist.c
@@ -159,6 +159,13 @@ listnode_add_after (struct list *list, struct listnode *pp, void *val)
   list->count++;
 }
 
+/* Move given listnode to tail of the list */
+void
+listnode_move_to_tail (struct list *l, struct listnode *n)
+{
+  LISTNODE_DETACH(l,n);
+  LISTNODE_ATTACH(l,n);
+}
 
 /* Delete specific date pointer from the list. */
 void

ospfd/ospf_packet.c
@@ -760,11 +760,6 @@ ospf_write (struct thread *thread)
   sockopt_iphdrincl_swab_htosys (&iph);
   ret = sendmsg (ospf->fd, &msg, flags);
   sockopt_iphdrincl_swab_systoh (&iph);
-  if (IS_DEBUG_OSPF_EVENT)
-    zlog_debug ("ospf_write to %s, "
-	       "id %d, off %d, len %d, interface %s, mtu %u:",
-	       inet_ntoa (iph.ip_dst), iph.ip_id, iph.ip_off, iph.ip_len,
-	       oi->ifp->name, oi->ifp->mtu);
   
   if (ret < 0)
     zlog_warn ("*** sendmsg in ospf_write failed to %s, "
@@ -796,14 +791,11 @@ ospf_write (struct thread *thread)
 
   /* Move this interface to the tail of write_q to
 	 serve everyone in a round robin fashion */
-  list_delete_node (ospf->oi_write_q, node);
+  listnode_move_to_tail (ospf->oi_write_q, node);
   if (ospf_fifo_head (oi->obuf) == NULL)
     {
       oi->on_write_q = 0;
-    }
-  else
-    {
-      listnode_add (ospf->oi_write_q, oi);
+      list_delete_node (ospf->oi_write_q, node);
     }
   
   /* If packets still remain in queue, call write thread. */

commit 1c6db0d2da34044ddfb42665fda8a3387ecc451d

lib: don't create circular lists (fixes 6d83113)
LISTNODE_DETACH doesn't clear out the node, and LISTNODE_ATTACH doesn't
set ->next (since it assumes a fresh/zeroed listnode).  As a result, the
new listnode_move_to_tail() created a nice circular list, in turn
crashing ospfd in ospf_write() later.

Reported-by: Martin Winter <mwinter@netdef.org>
Fixes: 6d83113 ("ospfd: Tweak previous iface RR write patch to avoid free/malloc & redundant log")
Cc: Paul Jakma <paul@opensourcerouting.org>
Signed-off-by: David Lamparter <equinox@opensourcerouting.org>
Acked-by: Greg Troxel <gdt@ir.bbn.com>
Acked-by: Dinesh Dutt <ddutt@cumulusnetworks.com>
Acked-by: Vincent JARDIN <vincent.jardin@6wind.com>

lib/linklist.h
@@ -113,6 +113,7 @@ extern void list_add_list (struct list *, struct list *);
 #define LISTNODE_ATTACH(L,N) \
   do { \
     (N)->prev = (L)->tail; \
+    (N)->next = NULL; \
     if ((L)->head == NULL) \
       (L)->head = (N); \
     else \