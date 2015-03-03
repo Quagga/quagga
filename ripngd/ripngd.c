ab90fc04a57b7b1d93ccddb8c9fbbf339a7ffc4c
David Lamparter <equinox@opensourcerouting.org>
2015-03-03 09:07:25 +0100

    *: add/cleanup initialisers
    There were some (inconsequential) warnings about uninitialised use of
    variables.  Also, in one case, sub-structs were mixed in initialisation,
    which doesn't quite work as intended.
    
    Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

--- ripngd/ripngd.c
+++ ripngd/ripngd.c
@@ -236,7 +236,7 @@ ripng_recv_packet (int sock, u_char *buf, int bufsize,
   struct msghdr msg;
   struct iovec iov;
   struct cmsghdr  *cmsgptr;
-  struct in6_addr dst;
+  struct in6_addr dst = { .s6_addr = { 0 } };
 
   /* Ancillary data.  This store cmsghdr and in6_pktinfo.  But at this
      point I can't determine size of cmsghdr */
