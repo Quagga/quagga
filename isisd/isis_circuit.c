52f02b47685bc823c4c75560175a27aab0bd6709
David Lamparter <equinox@opensourcerouting.org>
2015-04-10 09:14:30 +0200

    zebra, isisd: cast to unsigned char for ctypes
    ctype.h macros take int as arguments, but expect arguments to be in
    unsigned char's range.  Even though it probably works, this isn't
    correct on systems that have a signed char type.  Cast explicitly.
    
    Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

--- isisd/isis_circuit.c
+++ isisd/isis_circuit.c
@@ -397,7 +397,7 @@ isis_circuit_id_gen (struct interface *ifp)
    */
   for (i = 0; i < strlen (ifp->name); i++)
     {
-      if (isdigit(ifp->name[i]))
+      if (isdigit((unsigned char)ifp->name[i]))
         {
           if (start < 0)
             {
