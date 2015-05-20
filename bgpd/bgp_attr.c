c69698704806a9ac5035521b1820057097919227
Daniel Walton <dwalton@cumulusnetworks.com>
2015-05-19 18:03:43 -0700

    bgpd: Correct a few fuzz failures in BGP
    Testing revealed some issues with handling data input.
    This patch fixes those issues.
    
    Signed-off-by: Daniel Walton <dwalton@cumulusnetworks.com>

--- bgpd/bgp_attr.c
+++ bgpd/bgp_attr.c
@@ -2157,9 +2157,11 @@ bgp_attr_parse (struct peer *peer, struct attr *attr, bgp_size_t size,
 	{
 	  zlog (peer->log, LOG_WARNING, 
 		"%s: BGP type %d length %d is too large, attribute total length is %d.  attr_endp is %p.  endp is %p", peer->host, type, length, size, attr_endp, endp);
-	  bgp_notify_send (peer, 
-			   BGP_NOTIFY_UPDATE_ERR, 
-			   BGP_NOTIFY_UPDATE_ATTR_LENG_ERR);
+	  zlog_warn ("%s: BGP type %d length %d is too large, attribute total length is %d.  attr_endp is %p.  endp is %p", peer->host, type, length, size, attr_endp, endp);
+          bgp_notify_send_with_data (peer,
+                                     BGP_NOTIFY_UPDATE_ERR,
+                                     BGP_NOTIFY_UPDATE_ATTR_LENG_ERR,
+                                     startp, attr_endp - startp);
 	  return BGP_ATTR_PARSE_ERROR;
 	}
 	
