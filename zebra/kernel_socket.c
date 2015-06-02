d6cf5134c05a7890738411852d9357ee5bb322f3
David Lamparter <equinox@opensourcerouting.org>
2015-06-02 08:31:38 +0200

    zebra: fix VRF code for *BSD
    There seems to be no rtm_table in struct rt_msghdr, at least on the
    systems I have access to...
    
    Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

--- zebra/kernel_socket.c
+++ zebra/kernel_socket.c
@@ -962,7 +962,7 @@ rtm_read (struct rt_msghdr *rtm)
           || rtm->rtm_type == RTM_ADD
           || rtm->rtm_type == RTM_CHANGE)
         rib_add_ipv4 (ZEBRA_ROUTE_KERNEL, zebra_flags, &p, &gate.sin.sin_addr,
-                      NULL, 0, VRF_DEFAULT, rtm->rtm_table, 0, 0, SAFI_UNICAST);
+                      NULL, 0, VRF_DEFAULT, 0, 0, 0, SAFI_UNICAST);
       else
         rib_delete_ipv4 (ZEBRA_ROUTE_KERNEL, zebra_flags, &p,
                          &gate.sin.sin_addr, 0, VRF_DEFAULT, SAFI_UNICAST);
