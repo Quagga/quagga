a0d0b96396dd93c59802ad2cf1f2e59e1058e409
David Lamparter <equinox@opensourcerouting.org>
2015-06-02 07:54:00 +0200

    zebra: fix VRF netlink dependency for *BSD
    The VRF code added a dependency on sockaddr_nl, which obviously doesn't
    quite exist on the BSDs.
    
    Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

--- zebra/rib.h
+++ zebra/rib.h
@@ -355,6 +355,7 @@ struct rtadv
 };
 #endif /* RTADV && HAVE_IPV6 */
 
+#ifdef GNU_LINUX
 /* Socket interface to kernel */
 struct nlsock
 {
@@ -363,6 +364,7 @@ struct nlsock
   struct sockaddr_nl snl;
   const char *name;
 };
+#endif
 
 /* Routing table instance.  */
 struct zebra_vrf
@@ -385,9 +387,11 @@ struct zebra_vrf
   /* Static route configuration.  */
   struct route_table *stable[AFI_MAX][SAFI_MAX];
 
+#ifdef GNU_LINUX
   struct nlsock netlink;     /* kernel messages */
   struct nlsock netlink_cmd; /* command channel */
   struct thread *t_netlink;
+#endif
 
   /* 2nd pointer type used primarily to quell a warning on
    * ALL_LIST_ELEMENTS_RO
