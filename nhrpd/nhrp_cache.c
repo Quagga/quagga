Quagga/quagga

commit 17bcc8f1617b96557222294c289bcad4c325ab31

nhrpd: fix internal cache routes
When cache route is withdrawn, the type is invalid, and the internal
nhrp interface pointer was not removed properly. Move the nhrp cache
updating to happen from cache directly, and properly call the remove
when needed.

nhrpd/nhrp_cache.c
@@ -116,15 +116,28 @@ static void nhrp_cache_update_route(struct nhrp_cache *c)
 		sockunion2hostprefix(&c->remote_addr, &pfx);
 		netlink_update_binding(p->ifp, &c->remote_addr, &p->vc->remote.nbma);
 		nhrp_route_announce(1, c->cur.type, &pfx, c->ifp, NULL, c->cur.mtu);
+		if (c->cur.type >= NHRP_CACHE_DYNAMIC) {
+			nhrp_route_update_nhrp(&pfx, c->ifp);
+			c->nhrp_route_installed = 1;
+		} else if (c->nhrp_route_installed) {
+			nhrp_route_update_nhrp(&pfx, NULL);
+			c->nhrp_route_installed = 0;
+		}
 		if (!c->route_installed) {
 			notifier_call(&c->notifier_list, NOTIFY_CACHE_UP);
 			c->route_installed = 1;
 		}
-	} else if (c->route_installed) {
-		sockunion2hostprefix(&c->remote_addr, &pfx);
-		notifier_call(&c->notifier_list, NOTIFY_CACHE_DOWN);
-		nhrp_route_announce(0, c->cur.type, &pfx, NULL, NULL, 0);
-		c->route_installed = 0;
+	} else {
+		if (c->nhrp_route_installed) {
+			nhrp_route_update_nhrp(&pfx, NULL);
+			c->nhrp_route_installed = 0;
+		}
+		if (c->route_installed) {
+			sockunion2hostprefix(&c->remote_addr, &pfx);
+			notifier_call(&c->notifier_list, NOTIFY_CACHE_DOWN);
+			nhrp_route_announce(0, c->cur.type, &pfx, NULL, NULL, 0);
+			c->route_installed = 0;
+		}
 	}
 }
 

nhrpd/nhrp_route.c
@@ -69,7 +69,7 @@ static void nhrp_route_update_zebra(const struct prefix *p, union sockunion *nex
 	}
 }
 
-static void nhrp_route_update_nhrp(const struct prefix *p, struct interface *ifp)
+void nhrp_route_update_nhrp(const struct prefix *p, struct interface *ifp)
 {
 	struct route_node *rn;
 	struct route_info *ri;
@@ -89,9 +89,6 @@ void nhrp_route_announce(int add, enum nhrp_cache_type type, const struct prefix
 
 	if (zclient->sock < 0)
 		return;
-
-	if (nexthop == NULL && type >= NHRP_CACHE_DYNAMIC)
-		nhrp_route_update_nhrp(p, add ? ifp : NULL);
 
 	switch (type) {
 	case NHRP_CACHE_NEGATIVE:

nhrpd/nhrpd.h
@@ -239,6 +239,7 @@ struct nhrp_cache {
 	unsigned map : 1;
 	unsigned used : 1;
 	unsigned route_installed : 1;
+	unsigned nhrp_route_installed : 1;
 
 	struct notifier_block peer_notifier;
 	struct notifier_block newpeer_notifier;
@@ -335,6 +336,7 @@ int nhrp_nhs_add(struct interface *ifp, afi_t afi, union sockunion *proto_addr, 
 int nhrp_nhs_del(struct interface *ifp, afi_t afi, union sockunion *proto_addr, const char *nbma_fqdn);
 int nhrp_nhs_free(struct nhrp_nhs *nhs);
 
+void nhrp_route_update_nhrp(const struct prefix *p, struct interface *ifp);
 void nhrp_route_announce(int add, enum nhrp_cache_type type, const struct prefix *p, struct interface *ifp, const union sockunion *nexthop, uint32_t mtu);
 int nhrp_route_read(int command, struct zclient *zclient, zebra_size_t length);
 int nhrp_route_get_nexthop(const union sockunion *addr, struct prefix *p, union sockunion *via, struct interface **ifp);

commit b649ce419055c7b2883801198651d68a0ef6cef0

nhrpd: fix usage of unitialized prefix
Fixes: 17bcc8f16 "nhrpd: fix internal cache routes"

nhrpd/nhrp_cache.c
@@ -112,8 +112,9 @@ static void nhrp_cache_update_route(struct nhrp_cache *c)
 	struct prefix pfx;
 	struct nhrp_peer *p = c->cur.peer;
 
+	sockunion2hostprefix(&c->remote_addr, &pfx);
+
 	if (p && nhrp_peer_check(p, 1)) {
-		sockunion2hostprefix(&c->remote_addr, &pfx);
 		netlink_update_binding(p->ifp, &c->remote_addr, &p->vc->remote.nbma);
 		nhrp_route_announce(1, c->cur.type, &pfx, c->ifp, NULL, c->cur.mtu);
 		if (c->cur.type >= NHRP_CACHE_DYNAMIC) {