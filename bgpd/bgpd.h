fd24f31ca3b099dff87c2148396f143f652a0d2e
Daniel Walton <dwalton@cumulusnetworks.com>
2015-10-21 06:42:45 -0700

    Lower the default MRAI timer for iBGP peers to 0
    Signed-off-by: Daniel Walton <dwalton@cumulusnetworks.com>
    Reviewed-by:   Donald Sharp <sharpd@cumulusnetworks.com>

--- bgpd/bgpd.h
+++ bgpd/bgpd.h
@@ -749,7 +749,7 @@ struct bgp_nlri
 #define BGP_DEFAULT_KEEPALIVE                   60 
 #define BGP_DEFAULT_ASORIGINATE                 15
 #define BGP_DEFAULT_EBGP_ROUTEADV               30
-#define BGP_DEFAULT_IBGP_ROUTEADV                5
+#define BGP_DEFAULT_IBGP_ROUTEADV                0
 #define BGP_CLEAR_CONNECT_RETRY                 20
 #define BGP_DEFAULT_CONNECT_RETRY              120
 
