23757db11c5378521b7948847e17691f4fc35e0a
David Lamparter <equinox@opensourcerouting.org>
2016-02-24 06:26:02 +0100

    *: get rid of "MTYPE 0"
    A few places are using 0 in place of the MTYPE_* argument.  The
    following rewrite of the alloc tracking won't deal with that, so let's
    use MTYPE_TMP instead.
    
    Signed-off-by: David Lamparter <equinox@opensourcerouting.org>
    Acked-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- zebra/zserv.c
+++ zebra/zserv.c
@@ -1313,7 +1313,7 @@ zebra_client_create (int sock)
   struct zserv *client;
   int i;
 
-  client = XCALLOC (0, sizeof (struct zserv));
+  client = XCALLOC (MTYPE_TMP, sizeof (struct zserv));
 
   /* Make client input/output buffer. */
   client->sock = sock;
