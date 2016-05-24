3a72ea9b60a60ed01581e9037641731d44db9260
David Lamparter <equinox@opensourcerouting.org>
2016-05-24 18:26:55 +0200

    isisd: drop unused per-type metric values
    Expense, Error and Delay metrics never quite made it into the real
    world.  Either way isisd does nothing useful with them, so let's drop
    them from the code.  If someone wants to implement them, this patch can
    still be reverted.
    
    Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

--- isisd/isis_circuit.h
+++ isisd/isis_circuit.h
@@ -117,7 +117,7 @@ struct isis_circuit
   u_int16_t hello_multiplier[2];	/* l1HelloMultiplier */
   u_int16_t csnp_interval[2];	/* level-1 csnp-interval in seconds */
   u_int16_t psnp_interval[2];	/* level-1 psnp-interval in seconds */
-  struct metric metrics[2];	/* l1XxxMetric */
+  u_int8_t  metric[2];
   u_int32_t te_metric[2];
   struct mpls_te_circuit *mtc; /* Support for MPLS-TE parameters - see isis_te.[c,h] */
   int ip_router;		/* Route IP ? */
